4a1f3080e457f9e5d024907acf04f8476832e7c4
Adeil Mohammadzadah <145399269+Adeilmo226@users.noreply.github.com>
2026-02-11 20:31:48 -0600

    fix explore page

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { BookOpen, GraduationCap, Search, Sparkles, Volume2, Users, Heart } from
  */
 export default function Home() {
   return (
-    <div className="bg-white flex-1 flex flex-col">
+    <div className="bg-white min-h-[calc(100vh-5rem)] flex flex-col">
       {/* Section 1: Hero */}
       <section className="relative overflow-hidden">
         {/* Background Image Overlay */}
